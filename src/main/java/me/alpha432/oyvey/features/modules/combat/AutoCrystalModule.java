330e0334b8fd674b34f08738f1ac25876500531d
VideoMaple <bravesaintzach@gmail.com>
2026-07-03 11:38:25 -0400

    chatgpt changed
    th

---
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/AutoCrystalModule.java
@@ -0,0 +1,18 @@
+package me.alpha432.oyvey.features.modules.combat;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class AutoCrystalModule extends Module {
+
+    public AutoCrystalModule() {
+        super("AutoCrystal", "Automatically places and breaks end crystals", Category.COMBAT);
+    }
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
