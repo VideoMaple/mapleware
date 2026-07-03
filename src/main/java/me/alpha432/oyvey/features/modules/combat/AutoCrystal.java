f5909d5c40667396093ea18656910d1e0467a881
VideoMaple <bravesaintzach@gmail.com>
2026-07-03 11:24:42 -0400

    Update AutoCrystal.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/AutoCrystal.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/AutoCrystal.java
@@ -1,12 +1,18 @@
+package me.alpha432.oyvey.features.modules.combat;
+
+import me.alpha432.oyvey.features.modules.Module;
+
 public class AutoCrystal extends Module {
 
     public AutoCrystal() {
-        super("AutoCrystal", Category.COMBAT);
+        super("AutoCrystal", "Automatically places and breaks end crystals", Category.COMBAT);
     }
 
     @Override
-    public void onEnable() {}
+    public void onEnable() {
+    }
 
     @Override
-    public void onDisable() {}
+    public void onDisable() {
+    }
 }
